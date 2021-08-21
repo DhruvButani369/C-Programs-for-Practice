fc5a010265e42e7432c8f36762f1c85389558dd1
Dhruv Butani <developingwithdhruv@gmail.com>
2021-08-21 17:05:21 +0530

    Remove and add some lines in the comments section

--- Pattern.c
+++ Pattern.c
@@ -2,10 +2,9 @@
 
 int main(int argc, char const *argv[])
 {
-    //Write a C program to print a block F using hash (#), where the F has a height of six characters and width of five and four characters. And also to print a big 'C'. Go to the editor
     
-    //Expected Output:
-
+    
+    //Examples
      /*
      ######
      #
