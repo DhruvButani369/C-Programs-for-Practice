a3c66c613bd6583ca15cff66c5d82cc5d122ce0d
The Developer <89304288+thedeveloper9283@users.noreply.github.com>
2021-08-21 18:20:07 +0530

    Add "long double" Data Type

--- The_Display_Size_of_the_Different_Data_Type.c
+++ The_Display_Size_of_the_Different_Data_Type.c
@@ -10,6 +10,7 @@ int main(int argc, char const *argv[])
     printf("%lu\n", sizeof(float));
     printf("%lu\n", sizeof(char));
     printf("%lu\n", sizeof(short));
+    printf("%lu\n", sizeof(long double));
     
     return 0;
 }
